7cab2ae18ae2e925d91a87df78d451fcd3676d6d
Gary Leong <gwleong@gmail.com>
2014-07-28 19:37:28 +0000

    add first step in configuration

---
+++ recipes/configure.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: jiffy_mongodb
+# Recipe:: default
+#
+# Copyright (C) 2014 YOUR_NAME
+# 
+# All rights reserved - Do Not Redistribute
+#
+
+log "Checking for mongo.conf file ...." do
+    level :debug
+end
+
