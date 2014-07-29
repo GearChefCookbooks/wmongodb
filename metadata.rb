b0545a67d048a72a56ec31071891d27c9f12f741
Gary Leong <gwleong@gmail.com>
2014-07-29 06:59:35 +0000

    had to update mongodb version

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,8 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.3'
 
 depends "apt"
-depends 'mongodb', '~> 0.15.0'
+#depends 'mongodb', '~> 0.15.0'
+depends 'mongodb'
 depends 'monit'
 depends 'git'
 
