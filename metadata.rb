0622f11ba7af2c1e1d8a4ebf40dbea3ee293d85f
Gary Leong <gwleong@gmail.com>
2014-07-30 16:44:28 -0700

    changed metadata file and berksfile.lock

--- metadata.rb
+++ metadata.rb
@@ -4,11 +4,10 @@ maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
 description      'Installs/Configures wmongodb'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.6'
 
-
-
-depends 'mongodb', '~> 0.15.0'
+#depends 'mongodb', '~> 0.15.0'
+depends 'mongodb'
 depends 'monit'
 depends 'git'
 
