f7f02e5e444118298c23a25ec75e80a7a49257d1
Gary Leong <gwleong@gmail.com>
2014-10-20 19:53:55 -0700

    changed mongo role

--- roles/mongoreplica_attr.rb
+++ roles/mongoreplica_attr.rb
@@ -1,9 +1,9 @@
 name "mongoreplica_attr"
 description "mongoreplica_attr"
 default_attributes(
-   :mongodb => {
-       :config => { 
-          :replica_file => "/opt/shared/configs/mongo.yml"
+   :config => {
+       :shared => { 
+          :mongo_replica => "/opt/shared/configs/mongo.yml"
        },
        :ruby_gems => { 
           :mongo => "1.11.1",
